02a53c6798d45caeaf870ee23c743445402a2fa7
BDHU <bodunhu@utexas.edu>
2026-03-24 14:21:50 -0500

    Remove Slurm support, simplify scheduler to local jobs only

--- cuco/launch/__init__.py
+++ cuco/launch/__init__.py
@@ -1,12 +1,9 @@
-from .scheduler import JobScheduler, JobConfig
-from .scheduler import LocalJobConfig, SlurmDockerJobConfig, SlurmCondaJobConfig
+from .scheduler import JobScheduler, JobConfig, LocalJobConfig
 from .local import ProcessWithLogging
 
 __all__ = [
     "JobScheduler",
     "JobConfig",
     "LocalJobConfig",
-    "SlurmDockerJobConfig",
-    "SlurmCondaJobConfig",
     "ProcessWithLogging",
 ]
